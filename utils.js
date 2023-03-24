73c06461ebf4bee374603598330245b14869abf4
T1-Solo <128813665+T1-Solo@users.noreply.github.com>
2023-03-24 18:13:16 +0500

    Add files via upload

---
+++ utils.js
@@ -0,0 +1,15 @@
+function renderElement(element){
+    return (
+        document.querySelector(element)
+    )
+}
+function createTag(tag){
+    return (
+        document.createElement(tag)
+    )
+}
+function renderElementAll(element){
+    return (
+        document.querySelectorAll(element)
+    )
+}
